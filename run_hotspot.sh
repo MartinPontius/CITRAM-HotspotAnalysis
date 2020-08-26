cf5ae0e3cdfe7e9ded19e40577596295b17e1ef8
Martin Pontius <m.pontius@52north.org>
2020-08-26 11:43:01 +0200

    Update

--- run_hotspot.sh
+++ run_hotspot.sh
@@ -2,8 +2,13 @@
 
 
 FILEPATTERNS=("chemnitz_reduced" "chemnitz_KWall" "chemnitz_KW26" "chemnitz_KW27" "chemnitz_KW28" "chemnitz_KW29" "chemnitz_KW30" "chemnitz_KW31" "chemnitz_KW32" "chemnitz_KW33" "chemnitz_KW34")
+#FILEPATTERNS=("hamm_reduced" "chemnitz_KWall" "chemnitz_KW26" "chemnitz_KW27" "chemnitz_KW28" "chemnitz_KW29" "chemnitz_KW30" "chemnitz_KW31" "chemnitz_KW32" "chemnitz_KW33" "chemnitz_KW34")
+#FILEPATTERNS=("krefeld_reduced" "chemnitz_KWall" "chemnitz_KW26" "chemnitz_KW27" "chemnitz_KW28" "chemnitz_KW29" "chemnitz_KW30" "chemnitz_KW31" "chemnitz_KW32" "chemnitz_KW33" "chemnitz_KW34")
+
 
 INP_NB="hotspot_chemnitz.ipynb"
+#INP_NB="hotspot_hamm.ipynb"
+#INP_NB="hotspot_krefeld.ipynb"
 KG_KM=False
 
 for i in ${FILEPATTERNS[@]}
